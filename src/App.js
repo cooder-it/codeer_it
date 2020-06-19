d66c727166264ad6fd55c66fbd06434cd2c4e077
MasterPupil <54911685+MasterPupil@users.noreply.github.com>
2020-06-19 20:25:03 +0200

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+
+function getCookie(name) {
+  var cookieValue = null;
+  if (document.cookie && document.cookie !== '') {
+      var cookies = document.cookie.split(';');
+      for (var i = 0; i < cookies.length; i++) {
+          var cookie = cookies[i].trim();
+          // Does this cookie string begin with the name we want?
+          if (cookie.substring(0, name.length + 1) === (name + '=')) {
+              cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+              break;
+          }
+      }
+  }
+  return cookieValue;
+}
+
+function send_mail(e) {
+  const name = e.target.Name.value
+  const subject = e.target.Subject.value
+  const email = e.target.Email.value
+  const message = e.target.Message.value
+  
+  e.preventDefault()
+  const data = { Name: name, Subject: subject, Email: email,Message: message };
+  var csrftoken = getCookie('csrftoken');
+
+  fetch('http://127.0.0.1:8000/codeerit', {
+    method: 'POST', // or 'PUT'
+    headers: {
+      'Content-Type': 'application/json',
+      'X-CSRFToken' : csrftoken,
+      },
+    body: JSON.stringify(data),
+    })
+    .then(response => response.json())
+    .catch((error) => {
+      console.error('Error:', error);
+  });
+
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form onSubmit={send_mail} action="">
+          <input type="text" name="Name" placeholder="name"/>
+          <input type="text" name="Subject" placeholder="subject"/>
+          <input type="text" name="Email" placeholder="email"/>
+          <input type="text" name="Message" placeholder="message"/>
+          <button>Send</button>
+      </form>
+      
+    </>
   );
 }
 
