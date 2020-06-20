ef2fca046bd0a5ec0c0e52e07d464a298e08261e
MasterPupil <54911685+MasterPupil@users.noreply.github.com>
2020-06-20 18:55:30 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import "./static/css/style.css"
+import logo from "./static/images/Przezroczysty.jpg"
+import mainpic from "./static/images/img.jpg"
+import gosia from "./static/images/ja@2x.jpg"
+import kamil from "./static/images/ja@1x.jpg"
 
 
 function getCookie(name) {
@@ -26,7 +31,7 @@ function send_mail(e) {
   e.preventDefault()
   const data = { Name: name, Subject: subject, Email: email,Message: message };
   var csrftoken = getCookie('csrftoken');
-
+  console.log(JSON.stringify(data))
   fetch('http://127.0.0.1:8000/codeerit', {
     method: 'POST', // or 'PUT'
     headers: {
@@ -45,14 +50,57 @@ function send_mail(e) {
 function App() {
   return (
     <>
-      <form onSubmit={send_mail} action="">
+    <div className="wrapper">
+      <nav>
+        <ul>
+          <li><div className="logo"><img src={logo}/></div></li>
+          <li><a href="">Work</a></li>
+          <li><a href="">About</a></li>
+          <li><a href="">Let's talk</a></li>
+        </ul>
+      </nav>
+      <main>
+        <div className="Work">
+          <div>
+            <span>design</span><br/>
+            <span>& technology</span><br/>
+            <span>Minimalism is the ultimate form of sophistication.</span>
+          </div>
+          <div><img src={mainpic}/></div>
+        </div>
+
+        <div className="About">
+          <span>Who we are ?</span>
+          <div className="members">
+            <div>
+              <img src={gosia}/>
+              <p>Gosia</p>
+              <p>graphic designer, ilustrator and illustration lover focused on UI/UX learning. She try connects traditional art techniques with digital</p>
+            </div>
+            <div>
+              <img src={kamil}/>
+              <p>Kamil</p>
+              <p>bioinformatician with passion for coding, who studies at Poznań University of Technology. Web technologies are in his interes with pixel-perfect solutions</p>
+            </div>
+				  </div>
+			  </div>
+        <div className="letsTalk">
+          <h2>Let's talk!</h2>
+          <p>Feel free, write to us contact@codeer.it and tell as more about your project</p>
+        </div>
+        <footer>
+          <span>Codeer</span>
+          <span>Poznań 2020</span>
+        </footer>
+      </main>
+      <form onSubmit={send_mail} action="" method="POST">
           <input type="text" name="Name" placeholder="name"/>
           <input type="text" name="Subject" placeholder="subject"/>
           <input type="text" name="Email" placeholder="email"/>
           <input type="text" name="Message" placeholder="message"/>
-          <button>Send</button>
+          <button type="submit">Send</button>
       </form>
-      
+      </div>
     </>
   );
 }
