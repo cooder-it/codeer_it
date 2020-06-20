be2754090a83f830fd7f905b368227153d5b13da
MasterPupil <54911685+MasterPupil@users.noreply.github.com>
2020-06-20 21:31:57 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,46 +6,46 @@ import gosia from "./static/images/ja@2x.jpg"
 import kamil from "./static/images/ja@1x.jpg"
 
 
-function getCookie(name) {
-  var cookieValue = null;
-  if (document.cookie && document.cookie !== '') {
-      var cookies = document.cookie.split(';');
-      for (var i = 0; i < cookies.length; i++) {
-          var cookie = cookies[i].trim();
-          // Does this cookie string begin with the name we want?
-          if (cookie.substring(0, name.length + 1) === (name + '=')) {
-              cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-              break;
-          }
-      }
-  }
-  return cookieValue;
-}
+// function getCookie(name) {
+//   var cookieValue = null;
+//   if (document.cookie && document.cookie !== '') {
+//       var cookies = document.cookie.split(';');
+//       for (var i = 0; i < cookies.length; i++) {
+//           var cookie = cookies[i].trim();
+//           // Does this cookie string begin with the name we want?
+//           if (cookie.substring(0, name.length + 1) === (name + '=')) {
+//               cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+//               break;
+//           }
+//       }
+//   }
+//   return cookieValue;
+// }
 
-function send_mail(e) {
-  const name = e.target.Name.value
-  const subject = e.target.Subject.value
-  const email = e.target.Email.value
-  const message = e.target.Message.value
+// function send_mail(e) {
+//   const name = e.target.Name.value
+//   const subject = e.target.Subject.value
+//   const email = e.target.Email.value
+//   const message = e.target.Message.value
   
-  e.preventDefault()
-  const data = { Name: name, Subject: subject, Email: email,Message: message };
-  var csrftoken = getCookie('csrftoken');
-  console.log(JSON.stringify(data))
-  fetch('http://127.0.0.1:8000/codeerit', {
-    method: 'POST', // or 'PUT'
-    headers: {
-      'Content-Type': 'application/json',
-      'X-CSRFToken' : csrftoken,
-      },
-    body: JSON.stringify(data),
-    })
-    .then(response => response.json())
-    .catch((error) => {
-      console.error('Error:', error);
-  });
+//   e.preventDefault()
+//   const data = { Name: name, Subject: subject, Email: email,Message: message };
+//   var csrftoken = getCookie('csrftoken');
+//   console.log(JSON.stringify(data))
+//   fetch('http://127.0.0.1:8000/codeerit', {
+//     method: 'POST', // or 'PUT'
+//     headers: {
+//       'Content-Type': 'application/json',
+//       'X-CSRFToken' : csrftoken,
+//       },
+//     body: JSON.stringify(data),
+//     })
+//     .then(response => response.json())
+//     .catch((error) => {
+//       console.error('Error:', error);
+//   });
 
-}
+// }
 
 function App() {
   return (
